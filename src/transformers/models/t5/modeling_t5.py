24ae27cbb3dc4d56dc2f45e90f4b0e9fd4d5edcf
kailashs <kailashs@elementalcognition.com>
2021-04-08 11:39:02 -0400

    instance level loss weight

--- src/transformers/models/t5/modeling_t5.py
+++ src/transformers/models/t5/modeling_t5.py
@@ -1373,7 +1373,7 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
         r"decoder\.block\.0\.layer\.1\.EncDecAttention\.relative_attention_bias\.weight",
     ]
 
-    def __init__(self, config):
+    def __init__(self, config, custom_loss=None):
         super().__init__(config)
         self.model_dim = config.d_model
 
@@ -1398,6 +1398,8 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
         # Model parallel
         self.model_parallel = False
         self.device_map = None
+
+        self.custom_loss = custom_loss
 
     @add_start_docstrings(PARALLELIZE_DOCSTRING)
     def parallelize(self, device_map=None):
@@ -1462,6 +1464,7 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
         output_attentions=None,
         output_hidden_states=None,
         return_dict=None,
+        instance_weights=None,
     ):
         r"""
         labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
@@ -1577,8 +1580,11 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
 
         loss = None
         if labels is not None:
-            loss_fct = CrossEntropyLoss(ignore_index=-100)
-            loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
+            loss_fct = self.custom_loss if hasattr(self, 'custom_loss') and self.custom_loss is not None else CrossEntropyLoss(ignore_index=-100)
+            if instance_weights is not None:
+                loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1), instance_weights)
+            else:
+                loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
             # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
 
         if not return_dict:
